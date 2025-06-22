a1cad963f19bd0b24641f173e97d30e179d5c925
Yajush Khatter <yajush.khatter@gmail.com>
2025-06-22 11:31:47 +0530

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const { Server } = require("socket.io");
+
+const io = new Server(server);
 
 //routes
 app.get("/", (req, res)=>{
     res.send(
         "This is mern realtime board sharing app official server"
-    );
+    );  
+})
+
+io.on("connection", (socket)=>{
+    console.log("User Connected");
 })
 
 const port = process.env.PORT || 5000;
