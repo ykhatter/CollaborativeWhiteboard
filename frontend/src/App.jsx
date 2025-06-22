3740dc88460c990da7d0f3f946162c8a67f5af2d
Yajush Khatter <yajush.khatter@gmail.com>
2025-06-22 09:50:16 +0530

    added routing functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import Forms from './components/Forms'
-import './App.css'
+// import Forms from './components/Forms'
+// import './App.css'
 
-const App=() => {
+// const App=() => {
 
+//   return (
+//       <div className="container">
+//         <Forms/>
+//       </div>
+//   )
+// }
+
+// export default App
+
+import Forms from './components/Forms';
+import { Route, Routes } from "react-router-dom";
+import RoomPage from './pages/RoomPage'; 
+import './App.css';
+
+const App = () => {
   return (
-      <div className="container">
-        <Forms/>
-      </div>
-  )
-}
+    <div className="app-container">
+      <Routes>
+        <Route path="/" element={<Forms />} />
+        <Route path="/:roomId" element={<RoomPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
+
---
+++ frontend/src/pages/RoomPage/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const RoomPage = () => {
+    return(
+        <div className="row">
+            <h1 className="text-center py-5">White Board Sharing App</h1>    
+        </div>
+    );
+};
+
+export default RoomPage;
